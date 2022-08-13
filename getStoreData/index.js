726c6fefac2cb7a41ea1e0886089fe8a620511aa
Cole Horvat <colehorvat@pop-os.localdomain>
2022-08-12 21:21:02 -0700

    Added getStoreData timer function

---
+++ getStoreData/index.js
@@ -0,0 +1,20 @@
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    
+    if (myTimer.isPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+
+
+
+    context.log('JavaScript timer trigger function ran!', timeStamp);   
+};
+
+async function getEpicData() {
+
+}
+
+async function getSteamData() {
+    
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
 			<div className="container-fluid">
 				<a className="navbar-brand" href="#">
 					<img src={"https://i.etsystatic.com/20043299/r/il/25c5a5/2007544901/il_fullxfull.2007544901_akx5.jpg"} alt="" width="30" height="24" className="d-inline-block align-text-top" />
-						Free Game Tracker
+						Free Game Tracker 
 				</a>
 			</div>
 		</nav>
