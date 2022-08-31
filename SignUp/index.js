7357a3b01d1a2422e92da0c95ae8ae0813b54b45
Cole Horvat <colehorvat@pop-os.localdomain>
2022-08-31 15:40:31 -0700

    Added unique recipient id generation and add recipient request

--- SignUp/index.js
+++ SignUp/index.js
@@ -5,16 +5,71 @@ SignUp:
 - Adds user to Courier list
 */
 
+const dotenv = require('dotenv');
+
+dotenv.config()
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    //Initialize data from form
+
+    const FIRST_NAME = "Cole" //req.headers or req.body[path]
+    const EMAIL = process.env.TEST_EMAIL //req.headers or req.body[path]
+    const PHONE = process.env.TEST_PHONE //req.headers or req.body[path]
+    const CONTACT_PREF = ["Email"] //req.body[path]
+
+    //Create unique recipient ID
+    /*
+        ID = FGT-(Year)(Month)(Day)(Hour)(Minute)(Second)(4 random digits)
+    */
+
+    //Get Date
+    let currentDate = new Date();
+    let cDay = currentDate.getDate();
+    let cMonth = currentDate.getMonth() + 1;
+    let cYear = currentDate.getFullYear();
+
+    let dateString = cYear + "" + cMonth + "" + cDay + "";
+
+    //Get Time
+    let hour = currentDate.getHours();
+    let min = currentDate.getMinutes();
+    let sec = currentDate.getSeconds();
+
+    let timeString = hour + "" + min + "" + sec + ""
+
+    //Get 4 Random Digits
+    let digit = Math.floor(1000 + Math.random() * 9000);
+
+    //Create ID
+    const USER_ID = "FGT-" + dateString + timeString + digit 
+
+    //Create Courier recipient
+    const AUTH_KEY = process.env.AUTH_KEY
+
+    const profileOptions = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + AUTH_KEY
+        },
+        body: JSON.stringify({
+            "profile": {
+                "email": EMAIL,
+                "phone_number": PHONE
+            }
+        })
+    };
+
+    fetch('https://api.courier.com/profiles/0460766e-8463-4905-ae98-b72c7aef41d6', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: ""
     };
 }
