3aaf536bcdedc1cea73156341e53f81cbd69ead4
Cole <ColeHorvat@gmail.com>
2022-09-20 00:11:36 -0700

    Planned out sendcontroller function, created compareResult function to compare the old and new json data

---
+++ SendController/index.js
@@ -0,0 +1,40 @@
+import { CourierClient } from "@trycourier/courier";
+
+const AUTH_KEY = process.env.AUTH_KEY
+const GET_STORE_DATA_URL = "" //process.env.GET_STORE_DATA_URL
+
+const courier = CourierClient({ authorizationToken: AUTH_KEY });
+
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+
+    let oldResult = "" //Retrieve from database
+    let newResult = "" //Make request to GET_STORE_DATA_URL
+
+
+    let content = compareResult(oldResult, newResult)
+
+    if(content[0] || content[1]) {
+        // SEND TO LIST
+    }
+
+    if (myTimer.isPastDue) {
+        context.log('JavaScript is quite running late!');
+    }
+    context.log('JavaScript timer trigger function ran!', timeStamp);
+};
+
+function compareResult(oldRes, newRes) {
+	let result = []
+	newRes.forEach((store, sIndex) => {
+		store.forEach((game, gIndex) => {
+			if (oldRes[sIndex].some(compare => compare.title === game.title)) {
+				store[gIndex] = null;
+			}
+		})
+		let newStore = store.filter(x => x !== null)
+		result.push(newStore)
+	});
+
+	return result
+}
