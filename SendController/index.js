ff7c62dd3b7085d35979aea7f2a57b90d767fd2c
Cole <ColeHorvat@gmail.com>
2022-09-21 04:22:15 -0700

    Added logic for getting data from blob storage and apis, comparing that data, and sending notifications if theres a new promotion

--- SendController/index.js
+++ SendController/index.js
@@ -1,40 +1,91 @@
-import { CourierClient } from "@trycourier/courier";
+/* import { CourierClient } from "@trycourier/courier"; */
+const { CourierClient } = require("@trycourier/courier")
+const fetch = require("node-fetch")
+const { BlobServiceClient } = require('@azure/storage-blob')
 
 const AUTH_KEY = process.env.AUTH_KEY
-const GET_STORE_DATA_URL = "" //process.env.GET_STORE_DATA_URL
+const GET_STORE_DATA_URL = process.env.GET_STORE_DATA_URL_TEST
+const SAVED_DATA_URL = process.env.SAVED_DATA_URL
+const CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING
+
+const TEST_PHONE = process.env.TEST_PHONE
+const TEST_EMAIL = process.env.TEST_EMAIL
 
 const courier = CourierClient({ authorizationToken: AUTH_KEY });
 
 module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
+	var timeStamp = new Date().toISOString();
 
-    let oldResult = "" //Retrieve from database
-    let newResult = "" //Make request to GET_STORE_DATA_URL
+	let oldResult = await getStoredData()//Retrieve from database
+	let newResult =	await getRequest(GET_STORE_DATA_URL) //Make request to GET_STORE_DATA_URL
 
+	let content = await compareResult(oldResult, newResult)
+	console.log(content)
+	if (await content[0] || await content[1]) {
+		// SEND TO LIST AND ADD NEW RES TO DATABASE
+		const { requestId } = await courier.send({
+			message: {
+				to: {
+					email: TEST_EMAIL,
+					phone_number: TEST_PHONE
+				},
+				template: "SET3Q2V4WNMNPNG9C5FQ2P7GT3K8",
+				data: {
+					recipientName: "Cole",
+					steamGames: content[1],
+					epicGames: content[0],
+					cancelLink: "Cancel Link Placeholder"
+				},
+			},
+		});
 
-    let content = compareResult(oldResult, newResult)
+		uploadFile(await JSON.stringify(content))
+		context.log("NOTIFICATION SENT")
+	} else
+		context.log("NO NOTIFICATION")
 
-    if(content[0] || content[1]) {
-        // SEND TO LIST AND ADD NEW RES TO DATABASE
-    }
-
-    if (myTimer.isPastDue) {
-        context.log('JavaScript is quite running late!');
-    }
-    context.log('JavaScript timer trigger function ran!', timeStamp);
+	if (myTimer.isPastDue) {
+		context.log('JavaScript is quite running late!');
+	}
+	context.log('JavaScript timer trigger function ran!', timeStamp);
 };
 
-function compareResult(oldRes, newRes) {
+async function compareResult(oldRes, newRes) {
+	/* console.log(oldRes) */
 	let result = []
-	newRes.forEach((store, sIndex) => {
-		store.forEach((game, gIndex) => {
-			if (oldRes[sIndex].some(compare => compare.title === game.title)) {
-				store[gIndex] = null;
+	for (let s = 0; s < newRes.length; s++) {
+		for (let g = 0; g < newRes[s].length; g++) {
+			if (oldRes[s] && oldRes[s].some(compare => compare.title === newRes[s][g].title)) {
+				newRes[s][g] = null
 			}
-		})
-		let newStore = store.filter(x => x !== null)
+		}
+		let newStore = newRes[s].filter(x => x !== null)
 		result.push(newStore)
-	});
+	}
 
 	return result
+}
+
+async function getRequest(url) {
+	let res = await fetch(url, {
+		method: "GET",
+	})
+
+	return await res
+}
+
+async function getStoredData() {
+	let res = await getRequest(SAVED_DATA_URL)
+	return res.json()
+}
+
+async function uploadFile(data) {
+	const blobServiceClient = BlobServiceClient.fromConnectionString(CONNECTION_STRING)
+	const containerName = "store-data"
+	const containerClient = blobServiceClient.getContainerClient(containerName)
+
+	const blobName = "storeData.json"
+	const blobkBlobClient = containerClient.getBlockBlobClient(blobName)
+
+	const rep = await blobkBlobClient.upload(data, data.length)
 }
